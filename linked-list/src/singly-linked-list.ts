9d62b27ef33f6c8c9eac8a0e008276598703d567
Marcus Grant <marcusfg@gmail.com>
2018-07-11 10:13:36 -0400

    add old linked list parts

---
+++ linked-list/src/singly-linked-list.ts
@@ -0,0 +1,109 @@
+// TODO:
+// * Create singly linked list data structure. Don't use classes.
+// * Create append operation. It takes list (head) and new value.
+// * Test code using any tool you like
+// * For reference please use wiki article: https://en.wikipedia.org/wiki/Linked_list
+// * There are no penalties for using recursion
+
+export interface ILinkedList<T> {
+  data?: T;
+  next?: ILinkedList<T>;
+}
+
+type LL<T> = ILinkedList<T>;
+type LLO<T> = LL<T> | undefined;
+
+function traverse<T>(list: LL<T>, tillIndex?: number, hiddenIter = 0): LLO<T> {
+  if (list.next === undefined) {
+    if (!tillIndex) return list;
+    if (hiddenIter < tillIndex) return undefined;
+    return list;
+  }
+  if (hiddenIter === tillIndex) return list;
+  return traverse(list.next, tillIndex, hiddenIter + 1);
+}
+
+export function nodeAt<T>(list: LL<T>, index: number) {
+  // low range exception
+  if (index < 0) return undefined;
+
+  // empty list
+  if (!list) return undefined;
+
+  // handles the normal cases and above range case
+  return traverse(list, index);
+}
+
+function lastNode<T>(list: LL<T>): LLO<T> {
+  return traverse(list);
+}
+
+export function append<T>(a: LLO<T>, b: LLO<T>): LLO<T> {
+  // if the first portion is missing, then appending just returns the other part.
+  if (!a) return b;
+  if (!b) return a; // vice versa
+
+  const lastA = lastNode(a);
+  lastA.next = b;
+  return a;
+}
+
+export function insertNodeAt<T>(
+  list: LL<T>,
+  node: LL<T>,
+  index: number,
+): LLO<T> {
+
+  // below range exception, just return old list
+  if (index < 0) return undefined;
+
+  // get index -= 1 to link node before new one with
+  const nodeBefore = nodeAt<T>(list, index - 1);
+  // index is the location of the node that will follow the new one
+  const nodeAfter = nodeAt<T>(list, index);
+
+  // if index == 0, then this will go to the beginning
+  if (index === 0) {
+    // ... and if there's already something at index 0...
+    // ... move current node there to follow the new node being placed there.
+    if (nodeAfter) {
+      node.next = nodeAfter;
+      return node;
+    } // otherwise this is an empty list, just return the node as the start of it.
+    return node;
+  }
+
+  // now index is definetely > 0
+  // but we could still be out of the maximum range, in which case return undefined
+  if (!nodeBefore) return undefined;
+
+  // now there's definately a node before the new one to link to the new one
+  nodeBefore.next = node;
+
+  // and if there is a node where this one is being put, set it after the new one
+  // if it's undefined the new node will have it's 'next' undefined and it will be...
+  // ...the last element in the list
+  node.next = nodeAfter;
+
+  return list;
+}
+
+export function removeNodeAt<T>(list: LL<T>, index: number): LLO<T> {
+  if (index < 0) return list;
+  // we're interested in the two nodes around the node at the index.
+  const nodeBefore = nodeAt<T>(list, index - 1);
+  const nodeAfter = nodeAt<T>(list, index + 1);
+
+  if  (!nodeBefore) { // no node before index...
+    if (nodeAfter) { // then 'nodeAfter' is the new start of the list
+      return nodeAfter;
+    } // if there's no node before or after, then the last node is being removed
+    return undefined;
+  }
+
+  // now there's definitely a node before the index.
+  // so we definitely need to link the node before to the node after.
+  // if it exists, it will be linked, if not it will just be the end of the list
+  nodeBefore.next = nodeAfter;
+  return list;
+}
---
+++ linked-list/test/test-linked-lists.ts
@@ -0,0 +1,183 @@
+import {
+  append,
+  ILinkedList,
+  insertNodeAt,
+  nodeAt,
+  removeNodeAt,
+} from '../src/singly-linked-list';
+
+import { expect } from 'chai';
+
+type AnyList = ILinkedList<any>;
+type NumList = ILinkedList<number>;
+const emptyList: AnyList = {};
+const thirdNode: NumList = { data: 27, next: undefined };
+const secondNode: NumList = { data: 13, next: thirdNode };
+const firstNode: NumList = { data: 42, next: secondNode };
+const ll: NumList = firstNode;
+describe('linkedlist.nodeAt()', () => {
+  it('should get the correct node, when in range', () => {
+    const firstResult = nodeAt(ll, 0);
+    expect(firstResult).to.eql(firstNode);
+    const secondResult = nodeAt(ll, 1);
+    expect(secondResult).to.eql(secondNode);
+    const thirdResult = nodeAt(ll, 2);
+    expect(thirdResult).to.equal(thirdNode);
+  });
+  it('should return undef if out of range index given', () => {
+    expect(nodeAt(ll, 100)).to.equal(undefined);
+  });
+  it('should return undef if negative index given', () => {
+    expect(nodeAt(ll, -1)).to.equal(undefined);
+  });
+  it('should return undef if start of empty list given', () => {
+    expect(nodeAt(undefined, 0)).to.equal(undefined);
+  });
+});
+
+describe('linkedlist.insertNodeAt()', () => {
+  let testList: NumList;
+  const expectedList0: NumList = undefined; // empty
+  it('should return undefined if inserting below 0 index', () => {
+    // const list1 = insertNodeIntoListAtIndex(testList, testNode1, -1);
+    expect(insertNodeAt(testList, { data: -13, next: undefined }, -1)).to.eql(undefined);
+  });
+  // [] -> [12]
+  it('should add a node to an empty list', () => {
+    // insert @ 0 -> [12]
+    testList = insertNodeAt(testList, { data: 12, next: undefined }, 0);
+    expect(testList).to.eql({ data: 12, next: undefined });
+  });
+  // [10] -> [10, 12]
+  it('should add a node to a single member list @ beginning', () => {
+    // insert @ 0 -> [10, 12]
+    // console.log(testList);
+    testList = insertNodeAt(testList, { data: 10, next: undefined }, 0);
+    // console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!! RESULT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+    // console.log(testList);
+    expect(testList).to.eql({
+      data: 10, next: {
+        data: 12, next: undefined,
+      },
+    });
+  });
+  // [10, 12] -> [10, 12, 13]
+  it('should add a node to end of list', () => {
+    // insert @ 1 -> [10, 12, 13]
+    testList = insertNodeAt(testList, { data: 13, next: undefined }, 2);
+    expect(testList).to.eql({
+      data: 10, next: {
+        data: 12, next: {
+          data: 13, next: undefined,
+        },
+      },
+    });
+  });
+  // [10, 12, 13] -> [10, 11, 12, 13]
+  it('should add a node in the middle of the list', () => {
+    // insert @ 1 -> [10, 11, 12, 13]
+    expect(
+      insertNodeAt(testList, { data: 11, next: undefined }, 1))
+      .to.eql({
+        data: 10, next: {
+          data: 11, next: {
+            data: 12, next: {
+              data: 13, next: undefined,
+            },
+          },
+        },
+      });
+  });
+  it('should return undefined if out of maximum range', () => {
+    // insert @ 5 -> [10, 11, 12, 13]
+    expect(insertNodeAt(testList, { data: 14, next: undefined }, 5))
+      .to.equal(undefined);
+  });
+});
+
+describe('linkedlist.removeNodeAt()', () => {
+  let testList: NumList = {
+    data: 10, next: {
+      data: 11, next: {
+        data: 12, next: {
+          data: 13, next: undefined,
+        },
+      },
+    },
+  };
+  it('should return same list if index < 0', () => {
+    expect(removeNodeAt(testList, -1)).to.eql(testList);
+  });
+  it('should return correct list after removing 1st node', () => {
+    testList = removeNodeAt(testList, 0);
+    expect(testList).to.eql({
+      data: 11, next: {
+        data: 12, next: {
+          data: 13, next: undefined,
+        },
+      },
+    });
+  });
+  it('should return correct list after removing a surrounded node', () => {
+    testList = removeNodeAt(testList, 1);
+    expect(testList).to.eql({
+      data: 11, next: {
+        data: 13, next: undefined,
+      },
+    });
+  });
+  it('should return correct list after removing end node', () => {
+    testList = removeNodeAt(testList, 1);
+    expect(testList).to.eql({
+      data: 11, next: undefined,
+    });
+  });
+  it('should return same list after trying to remove a node after end node', () => {
+    testList = removeNodeAt(testList, 1);
+    expect(testList).to.eql({
+      data: 11, next: undefined,
+    });
+  });
+  it('should return undefined after removing only remaining node', () => {
+    testList = removeNodeAt(testList, 0);
+    expect(testList).to.eql(undefined);
+  });
+
+  describe('linkedlist.append()', () => {
+    const testA: ILinkedList<string> = {
+      data: 'a', next: {
+        data: 'bc', next: undefined,
+      },
+    };
+    const testB: ILinkedList<string> = {
+      data: 'x', next: {
+        data: 'y', next: {
+          data: 'z', next: undefined,
+        },
+      },
+    };
+    it('should return undefined if both params are', () => {
+      expect(append(undefined, undefined)).to.equal(undefined);
+    });
+    it('should return 1st arg if 2nd is undefined', () => {
+      expect(append(Object.assign(testA), undefined)).to.eql(testA);
+    });
+    it('should return 2nd arg if 1st is undefined', () => {
+      expect(append(undefined, Object.assign(testB))).to.eql(testB);
+    });
+    it('should be able to combine two lists in order', () => {
+      const expectedAppend: ILinkedList<string> = {
+        data: 'a', next: {
+          data: 'bc', next: {
+            data: 'x', next: {
+              data: 'y', next: {
+                data: 'z', next: undefined,
+              },
+            },
+          },
+        },
+      };
+      expect(append(testA, testB)).to.eql(expectedAppend);
+    });
+  });
+});
